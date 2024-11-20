39b5299251384fa4c0d1f1c039604933b371fa43
hiukyu <vblue7@naver.com>
2024-11-20 14:20:24 +0900

    시큐리 설정 레이아웃 설정 아이템 repository 메소드들 생산

--- src/main/java/com/example/shoppro/entity/Item.java
+++ src/main/java/com/example/shoppro/entity/Item.java
@@ -1,13 +1,10 @@
 package com.example.shoppro.entity;
 
 import com.example.shoppro.constant.ItemSellStatus;
+import com.example.shoppro.entity.base.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -15,33 +12,29 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name="item")
-public class Item {
+@Table(name = "item")
+public class Item extends BaseEntity {
 
     @Id
-    @Column(name="item_id")
+    @Column(name = "item_id")       //테이블에서 매핑될 컬럼
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;            //상품 코드
 
-    private Long id; //상품코드
+    @Column(nullable = false, length = 50)
+    private String itemNm;      //상품명
 
-    @Column(nullable = false,length = 50)
-    private String itemNm; //상품명
-    @Column(name="price", nullable = false)
-    private int price;  //가격
+    @Column(name = "price", nullable = false)
+    private int price;          //가격
+
     @Column(nullable = false)
-    private int stockNumber;    //재고수량
+    private int stockNumber;    // 재고수량
 
-    @Lob
+    @Lob            // 텍스트 많이
     @Column(nullable = false)
-    private String itemDetail;  //상품상세설명
+    private String itemDetail;  //상품 상세설명
 
-    @Enumerated(EnumType.STRING)
-    private ItemSellStatus itemSellStatus; //상품판매상태
-    private LocalDateTime regTime;          //상품등록시간
-    private LocalDateTime updateTime;       //상품수정시간
-
-
-
-
+    @Enumerated(EnumType.STRING)        //enum 가지고 만듬 YES/NO , SELL/SOLD_OUT
+    private ItemSellStatus itemSellStatus;      // 상품 판매 상태
+    
 
 }
